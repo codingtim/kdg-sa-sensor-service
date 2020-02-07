457c4319f72ac33daabfd9f2ff565d2ebd7a1ccd
Tim Boeckstaens <boeckstaenstim@gmail.com>
2020-02-07 19:28:09 +0100

    add logging with slf4j and logback

--- src/test/kotlin/be/codingtim/velo/sensor/service/FirstTest.kt
+++ src/test/kotlin/be/codingtim/velo/sensor/service/FirstTest.kt
@@ -2,11 +2,17 @@ package be.codingtim.velo.sensor.service
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 class FirstTest {
+
+    private val logger: Logger = LoggerFactory.getLogger(this.javaClass)
+
     @Test
     internal fun firstTest() {
         val classUnderTest = ClassUnderTest(true)
+        logger.info("running first test")
         assertEquals(true, classUnderTest.runs)
     }
 }
