06a746c540d9cb3722b7722254f72b9859bc7f11
Tim Boeckstaens <boeckstaenstim@gmail.com>
2020-02-08 08:52:56 +0100

    add spring webflux test

---
+++ src/test/kotlin/be/codingtim/velo/sensor/service/web/HelloControllerTest.kt
@@ -0,0 +1,19 @@
+package be.codingtim.velo.sensor.service.web
+
+import org.junit.jupiter.api.Test
+import org.springframework.test.web.reactive.server.WebTestClient
+
+internal class HelloControllerTest {
+
+    private val controller = HelloController()
+    private val client = WebTestClient.bindToController(controller).build()
+
+    @Test
+    internal fun hello() {
+        client
+                .get()
+                .uri("/test")
+                .exchange()
+                .expectStatus().is2xxSuccessful
+    }
+}
