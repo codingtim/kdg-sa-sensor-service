5d26a06420c69d5f6c9fb667460589c7ca785d99
Tim Boeckstaens <boeckstaenstim@gmail.com>
2020-02-09 09:54:47 +0100

    add controller test for POST with body

--- src/test/kotlin/be/codingtim/velo/sensor/service/web/HelloControllerTest.kt
+++ src/test/kotlin/be/codingtim/velo/sensor/service/web/HelloControllerTest.kt
@@ -1,6 +1,10 @@
 package be.codingtim.velo.sensor.service.web
 
+import be.codingtim.velo.sensor.service.web.HelloController.Request
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders.CONTENT_TYPE
+import org.springframework.http.MediaType.APPLICATION_JSON_VALUE
 import org.springframework.test.web.reactive.server.WebTestClient
 
 internal class HelloControllerTest {
@@ -17,5 +21,17 @@ internal class HelloControllerTest {
                 .expectStatus().is2xxSuccessful
                 .expectBody()
                 .jsonPath("$.message").isEqualTo("test message")
+    }
+
+    @Test
+    internal fun post() {
+        client
+                .post()
+                .uri("/test")
+                .bodyValue("{\"message\": \"post some data\"}")
+                .header(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .exchange()
+                .expectStatus().isAccepted
+        assertEquals(Request("post some data"), controller.request)
     }
 }
